8bc2c5be5553a410edc3a124681f1c2795117918
EL Gabo <harcot.sys@gmail.com>
2022-07-22 22:23:15 -0500

    Este es un if

---
+++ if.js
@@ -0,0 +1,11 @@
+var edad = 18;
+if(edad == 18){
+    console.log("Igual a 18");
+} else if(edad > 18){
+    console.log("Ya has votado")
+} else {
+    console.log("No puedes votar");
+}
+
+var numero = 1;
+var resultado = numero === 1 ? "Si Soy un uno" : "No, no soy un uno";
