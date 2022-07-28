faec1f326ff1a8b838c3dc2ae08df8b3cf05e360
EL Gabo <harcot.sys@gmail.com>
2022-07-27 22:48:43 -0500

    Chuta ojala valga'

---
+++ js-practico/script.js
@@ -0,0 +1,26 @@
+// h1 {color: red}
+// . parrafito {.....}
+//#pid {...}
+//console.log('Hello Wordl');
+
+const h1 = document.querySelector('h1');
+//const p = document.querySelector('p');
+const p = document.querySelectorAll('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+//console.log(h1);
+//console.log(h1.ATTRIBUTE_NODE);
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input,
+}
+)
+
+
