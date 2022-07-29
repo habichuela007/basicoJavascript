110cc093ac3ab9768d9ef441e205b6932fbf469b
EL Gabo <harcot.sys@gmail.com>
2022-07-28 21:45:06 -0500

    Se acaba lo básico

--- js-practico/script.js
+++ js-practico/script.js
@@ -1,26 +1,23 @@
-// h1 {color: red}
-// . parrafito {.....}
-//#pid {...}
-//console.log('Hello Wordl');
-
 const h1 = document.querySelector('h1');
-//const p = document.querySelector('p');
-const p = document.querySelectorAll('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+const form = document.querySelector('#form')
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btnCalculate = document.querySelector('#btnCalculate');
+const pResult = document.querySelector('#result');
 
-//console.log(h1);
-//console.log(h1.ATTRIBUTE_NODE);
-console.log(input.value);
+//Pilas, cuando llamamos a la función desde HTML SI necesitamos paréntesis
+//EN el javascript NO debido a que javascript interpreta que debe ejecutar una función ya ejecutada (va a agregar otros paréntesis)
+//btnCalculate.addEventListener('click',btnOnClick);
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
+form.addEventListener('submit',btnOnClick)
+
+function btnOnClick(event){
+    console.log(event);
+    event.preventDefault();
+    const sumaInputs = parseInt(input1.value)  + parseInt(input2.value);
+    //console.log('Escuchando el evento de clic');
+    //console.log(input1.value + input2.value);
+    //console.log(Number(input1.value) + Number(input2.value));
+    //console.log(parseInt(input1.value) + parseInt(input2.value));
+    pResult.innerText = "Resultado " + sumaInputs;
 }
-)
-
-
